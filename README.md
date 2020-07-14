b9fba63a22fee40a54b89718e89e8bff5e1b9b97
darrida <darrida.py@gmail.com>
2020-07-14 08:26:46 -0500

    cdc api poc

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # covid-19-data-aggergation
 IN PROGRESS: Project to aggerate covid-19 data from two or more sources through data pipelines and present data.
+
+## APIs
+
+- CDC: https://open.cdc.gov/apis.html
--- api_downloads.py
+++ api_downloads.py
@@ -5,29 +5,60 @@ import pprint
 
 # PyPI
 import requests
+import pandas
 
+uri = 'https://data.cdc.gov/resource/muzy-jte6.json'
 state = 'Illinois'
+output = 'death_weekly_by_state.json'
 
-def api_death_weekly_by_state(state: str, year: int, week: int):
-    url = f'https://data.cdc.gov/resource/muzy-jte6.json' \
+
+def api_death_weekly_by_state(state: str, year: int, week: int, uri=uri):
+    url = uri \
         + f'?jurisdiction_of_occurrence={state}' \
         + f'&mmwryear={str(year)}' \
         + f'&mmwrweek={str(week)}'
     results = requests.get(url)
     return results.json()
 
-total_difference = 0
 
-try:
-    for i in range(1, 52):
-        data1 = api_death_weekly_by_state(state=state, year=2019, week=i)
-        data2 = api_death_weekly_by_state(state=state, year=2020, week=i)
-        difference = int(data2[0]['all_cause']) - int(data1[0]['all_cause'])
-        total_difference += difference
-        pprint.pprint(data1[0]['week_ending_date'] + ': ' + data1[0]['all_cause'] + ' => ' + data2[0]['week_ending_date'] + ': ' + data2[0]['all_cause'] + ' | Diff: ' + str(difference))
-except IndexError as e:
-    print(f'{i -1} is last week reported.')
-    print(f'Total difference: {total_difference}')
 
-# with open('death_weekly_by_stat.json', 'w') as outfile:
-#     json.dump(data, outfile)
+def api_death_compare(state: str, year1: int, year2: int):
+    totals = {
+        'year1': 0,
+        'year2': 0,
+        'difference': 0,
+        'total_diff': 0
+    }
+    try:
+        for i in range(1, 52):
+            data1 = api_death_weekly_by_state(state=state, year=year1, week=i)
+            data2 = api_death_weekly_by_state(state=state, year=year2, week=i)
+            #difference = int(data2[0]['all_cause']) - int(data1[0]['all_cause'])
+            totals['difference'] = int(data2[0]['all_cause']) - int(data1[0]['all_cause'])
+            totals['total_diff'] += int(totals['difference'])
+            totals['year1'] += int(data1[0]['all_cause'])
+            totals['year2'] += int(data2[0]['all_cause'])
+            #total_difference += difference
+            print(
+                data1[0]['week_ending_date'], ':', data1[0]['all_cause'], '=>', data2[0]['week_ending_date'] + ': ' + data2[0]['all_cause'], 
+                '| TOTALS - year1:', totals['year1'], '- year2:', totals['year2'],
+                '| Diff:', totals['difference']
+            ) 
+    except IndexError:
+        print(f'{i -1} is last week reported.')
+        total_diff = totals['total_diff']
+        print(f'Total difference: {total_diff}')
+
+
+def api_death_all_data_json(output_name_location: str, uri=uri):
+    results = requests.get(uri)
+    data = results.json()
+    with open(output_name_location, 'w') as outfile:
+        json.dump(data, outfile)
+
+
+def json_to_csv(json_input: str):
+    df = pandas.read_json(json_input)
+    df.to_csv('death_weekly_by_state.csv')
+
+api_death_compare('Illinois', 2019, 2020)
